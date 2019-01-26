9d431ab69334a6cb0f9e75dcac1f4ad49307414e
TowhidAust <towhidaustcse33@gmail.com>
2019-01-27 02:14:10 +0600

    switchnamehandler

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,13 +4,33 @@ import './App.css'
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max", age:21},
+      {name: "Manu", age: 31},
+      {name: "Stephanie", age: 40}],
+      
+      other: "Some value"
+  }
+
+  switchNameHandler = () => {
+    console.log("this is switch name handler");
+    this.setState({
+      persons: [
+        {name: "Max", age:21},
+        {name: "Manu", age: 31},
+        {name: "Pen", age: 50}]  
+    })
+  }
+
   render () {
     return (
       <div className='App'>
         <h1>Hi this is my first React App</h1>
-        <Person name="Max" age = "20" />
-        <Person name="Manu" age = "30" />
-        <Person name="Stephanie" age = "40"> My hobies: Racing </Person>
+        <button onClick = {this.switchNameHandler}>Switch Name</button>
+        <Person name = {this.state.persons[0].name} age ={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age = {this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age = {this.state.persons[2].age}> My hobies: Racing </Person>
       </div>
     )
   }
