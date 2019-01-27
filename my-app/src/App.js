1a27c39051d8ebc1f25ca53917e1ac9d2da70d96
TowhidAust <towhidaustcse33@gmail.com>
2019-01-27 23:17:29 +0600

    listing the person using map function.
    map function basically includes the array of the state.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -53,20 +53,10 @@ class App extends Component {
     if(this.state.showPersons){
       persons = (
         <div>
-            <Person 
-            name = {this.state.persons[0].name} 
-            age ={this.state.persons[0].age}
-            click = {this.switchNameHandler.bind(this, "FountainPen")} 
-            />
-
-            <Person 
-            name={this.state.persons[1].name} 
-            age = {this.state.persons[1].age} />
-
-            <Person 
-            name={this.state.persons[2].name} 
-            age = {this.state.persons[2].age}
-            change={this.nameChangeHandler}> My hobies: Racing </Person>
+          {this.state.persons.map(person => {
+            return <Person name = {Person.name} age = {person.age}/>
+          })}
+            
         </div>
       )
     }
