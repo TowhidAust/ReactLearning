9d53a12874b6186c0cfe965542e3913340d155fc
TowhidAust <towhidaustcse33@gmail.com>
2019-04-20 23:04:45 +0600

    commit react

--- my-app/src/containers/App.js
+++ my-app/src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 // import logo from './logo.svg';
 import './App.css'
-import Person from './Person/Person';
+import Person from '../components/Persons/Person';
 import Radium, {StyleRoot} from 'radium';
 
 class App extends Component {
